bda4c84f46afadbe856a71d30cc48716d35e459d
Ушаков Владимир <aurel.vu@ya.ru>
2021-12-13 17:18:17 +0300

    changed db url

--- app/config/heroku_config.py
+++ app/config/heroku_config.py
@@ -1,9 +1,6 @@
-import os
-
-
 class HerokuConfig(object):
     SECRET_KEY = "top secret"
     JWT_ACCESS_LIFESPAN = {"hours": 24}
     JWT_REFRESH_LIFESPAN = {"days": 30}
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = "postgresql://zskisktcgpcjig:6aaab573a652d0263acd8de16476f5ff4a98cb71534d76fddb7b62062c30a6d7@ec2-52-48-137-75.eu-west-1.compute.amazonaws.com:5432/d5e4scnlqnmnda"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
